a4042e025ef59e7a00a711eec73327385d734e31
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-09-06 13:25:19 -0300

    refactor: agrega atribución y enlace a bruno api

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,15 @@
 export function Sidebar () {
   return (
-    <aside class='h-full w-64 border-r border-base-content/20 bg-base-100'></aside>
+    <aside class='h-full w-64 flex flex-col border-r border-base-content/20 bg-base-100'>
+      <div class='mt-auto text-sm text-center pb-4'>
+        Diseño basado en&nbsp;
+        <a
+          href='https://www.usebruno.com/'
+          class='link link-accent'
+        >
+          Bruno
+        </a>
+      </div>
+    </aside>
   )
 }
