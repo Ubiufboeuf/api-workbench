c4d403439b500b738b7209b245723ec2721aa609
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-09-06 12:46:35 -0300

    chore: reconfigura eslint y tsconfig para agregar soporte y evitar conflictos en producción sobre el mismo eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,22 +1,61 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import pluginReact from 'eslint-plugin-react'
 import json from '@eslint/json'
-import { defineConfig } from 'eslint/config'
+import pluginReact from 'eslint-plugin-react'
 import stylistic from '@stylistic/eslint-plugin'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  { files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
+  // Archivos e ignorados
   {
+    ignores: ['dist/**', 'node_modules/**']
+  },
+
+  // Base JS
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser }
+  },
+
+  // TypeScript
+  ...tseslint.configs.recommended,
+
+  // React/Preact base
+  {
+    files: ['src/**/*.{jsx,tsx}'],
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: '16.0'
+      }
+    }
+  },
+
+  // Archivos JSON
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended']
+  },
+
+  // Reglas personalizadas + Stylistic + React rules
+  {
+    files: ['src/**/*.{js,jsx,ts,tsx}', 'eslint.config.ts'],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: new URL('.', import.meta.url).pathname
+      }
+    },
     plugins: {
-      '@stylistic': stylistic
+      '@stylistic': stylistic,
+      react: pluginReact
     },
     rules: {
-      'react/react-in-jsx-scope': 'off',
       'no-empty-pattern': 'off',
       '@stylistic/quotes': ['error', 'single'],
       '@stylistic/jsx-quotes': ['error', 'prefer-single'],
@@ -26,8 +65,8 @@ export default defineConfig([
       '@typescript-eslint/consistent-type-imports': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
       'space-before-function-paren': ['error', 'always'],
-      'react/no-unknown-property': ['error', { ignore: ['class'] }],
-      '@typescript/no-unused-vars': 'warn'
+      'react/react-in-jsx-scope': 'off',
+      'react/no-unknown-property': ['error', { ignore: ['class'] }]
     }
   }
 ])
