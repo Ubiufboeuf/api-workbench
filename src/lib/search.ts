1eeacae3ec286dc982bf9bea5c6f04d028b5124c
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-09-07 14:50:15 -0300

    feat: mejora el sistema de búsqueda de datos

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,13 +1,36 @@
 import { useResponseStore } from '../stores/responseStore'
 
-export async function search (query: string) {
-  let isValidUrl
+function getProtocol (query: string) {
   try {
-    isValidUrl = new URL(query)
-  } catch {/* empty */}
+    return new URL(query).protocol
+  } catch {
+    return
+  }
+}
 
-  if (!isValidUrl) return
+async function Fetch (url: string | URL, protocol?: string) {
+  if (!protocol || protocol === 'localhost:') {
+    return Promise.any([
+      fetch(`http://${url}`),
+      fetch(`https://${url}`)
+    ])
+  }
   
-  const res = await fetch(query) 
-  useResponseStore.setState({ res })
+  if (protocol.startsWith('http')) {
+    return fetch(url)
+  }
+
+  // if ws | sql | ...
+}
+
+export async function search (query: string) {
+  const protocol = getProtocol(query)
+  
+  let url
+  try {
+    url = new URL(query)
+  } catch { /* empty */ }
+  
+  const res = await Fetch(url ?? query, protocol)
+  useResponseStore.setState({ res })  
 }
