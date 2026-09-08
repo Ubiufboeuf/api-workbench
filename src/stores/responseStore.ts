08d3afb3037b141c11d7c326766c34ee1841c185
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-09-08 19:27:53 -0300

    feat: agrega varios datos a la store global de response

--- src/stores/responseStore.ts
+++ src/stores/responseStore.ts
@@ -1,11 +1,30 @@
 import { create } from 'zustand'
+import type { Display, ResponseType } from '../types/responseTypes'
 
 interface ResponseStore {
   res: Response | null
   setRes: (res: Response | null) => void
+
+  data: any
+  setData: (data: any) => void
+
+  responseType: ResponseType | undefined
+  setResponseType: (responseType: ResponseType) => void
+
+  display: Display | null
+  setDisplay: (display: Display | null) => void
 }
 
 export const useResponseStore = create<ResponseStore>((set) => ({
   res: null,
-  setRes: (res) => set({ res })
+  setRes: (res) => set({ res }),
+
+  data: null,
+  setData: (data) => set({ data }),
+
+  responseType: undefined,
+  setResponseType: (responseType) => set({ responseType }),
+
+  display: null,
+  setDisplay: (display) => set({ display })
 }))
